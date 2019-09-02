5c569f8a2c175a6712ff1b97ac0bdade9d54a428
codicus <rabalan83@gmail.com>
2019-09-02 23:06:01 +0700

    Отключен Hibernate, переход на JPA

--- HelloServletAndDBs/src/main/java/dao/UserDao.java
+++ HelloServletAndDBs/src/main/java/dao/UserDao.java
@@ -11,10 +11,45 @@ import java.util.List;
 
 public class UserDao {
 
-    public int register(User u) throws Exception{
+    @Resource
+    UserTransaction utx;
+
+    public int regi() {
+        int i = 0;
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("connDB");
+        EntityManager em = emf.createEntityManager();
+        User cat = new User();
+        cat.setEmail("mail");
+        cat.setPassword("pass");
+        cat.setName("name");
+
+        try {
+            utx.begin();
+            em.persist(cat);
+            em.flush();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (SystemException se) {
+                se.printStackTrace();
+            }
+
+        }
+
+
+
+
+        return i;
+    }
+
+    public static int register(User u) throws Exception{
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("connDB");
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
+
+
 
         int i = 0;
         i=1;
--- HelloServletAndDBs/src/main/webapp/register.jsp
+++ HelloServletAndDBs/src/main/webapp/register.jsp
@@ -1,10 +1,13 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@page import="dao.UserDao"%>
+<%@ page import="dao.User" %>
 <jsp:useBean id="obj" class="dao.User"/>
 <jsp:setProperty property="*" name="obj"/>
 
 <%
-    int i=UserDao.register(obj);
+//    int i=UserDao.register(obj);
+    UserDao ud = new UserDao();
+    int i = ud.regi();
     if(i>0)
         System.out.print("You are successfully registered");
 %>
